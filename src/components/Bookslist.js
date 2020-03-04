3a3bb7e5b3f1375e2dd951c5481251ab05797c62
Federica Colapaoli <federica.colapaoli@coresecure.com>
2020-03-04 16:47:45 -0500

    transformed Bookslist component from class to funct+useContext

--- src/components/Bookslist.js
+++ src/components/Bookslist.js
@@ -1,24 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import {ThemeContext} from '../context/ThemeContext'
 
-class Bookslist extends Component {
 
-    static contextType = ThemeContext;
-    render() { 
-        //this works too, but destructuring is better
-        // const theme = this.context.isLightTheme ? this.context.light : this.context.dark;
-        const {isLightTheme, light, dark} = this.context;
-        const theme = isLightTheme ? light : dark;
-        return (
-            <div className="book-list" style={{background: theme.ui, color: theme.syntax}}>
-                <ul>
-                    <li style={{background: theme.background}}>Volare</li>
-                    <li style={{background: theme.background}}>Se Telefonando</li>
-                    <li style={{background: theme.background}}>Acido Acida</li>
-                </ul>
-            </div>
-          );
-    }
+const Booklist = () => {
+
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+    return ( 
+        <div className="book-list" style={{background: theme.ui, color: theme.syntax}}>
+            <ul>
+                <li style={{background: theme.background}}>Volare</li>
+                <li style={{background: theme.background}}>Se Telefonando</li>
+                <li style={{background: theme.background}}>Acido Acida</li>
+            </ul>
+        </div>
+     );
 }
  
-export default Bookslist;
+export default Booklist;
+
+// class Bookslist extends Component {
+
+//     static contextType = ThemeContext;
+//     render() { 
+//         //this works too, but destructuring is better
+//         // const theme = this.context.isLightTheme ? this.context.light : this.context.dark;
+//         const {isLightTheme, light, dark} = this.context;
+//         const theme = isLightTheme ? light : dark;
+//         return (
+//             <div className="book-list" style={{background: theme.ui, color: theme.syntax}}>
+//                 <ul>
+//                     <li style={{background: theme.background}}>Volare</li>
+//                     <li style={{background: theme.background}}>Se Telefonando</li>
+//                     <li style={{background: theme.background}}>Acido Acida</li>
+//                 </ul>
+//             </div>
+//           );
+//     }
+// }
+ 
+// export default Bookslist;
